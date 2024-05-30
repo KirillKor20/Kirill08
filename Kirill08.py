8da8aa775db428bc5f6228f4624a15e903efdd07
KirillKor20 <vwer7743@mail.ru>
2024-05-30 06:39:54 +0300

    Add files via upload

---
+++ Kirill08.py
@@ -0,0 +1,21 @@
+#my_dict = {'Kirill': 2000, 'Nikita': 1999,'Kristina': 2002}
+#my_dict.update({'Katya':  1997,
+#               'Lera': 2004})
+#print(my_dict)
+#print(my_dict.get('Nikita'))
+#print(my_dict.get('Natasha'))
+#print(my_dict)
+#b = my_dict.pop('Kirill')
+#print(my_dict)
+#print(b)
+
+my_set = {23,42,53,12,23,42,53,12, 'Finish', False , 'pineapple',(32,22)}
+my_set = [67,86,34,54,67,86,34,54]
+my_set = set(my_set)
+print(my_set)
+print(my_set.remove(86))
+print(my_set)
+print(my_set.add(82))
+print(my_set)
+
+
